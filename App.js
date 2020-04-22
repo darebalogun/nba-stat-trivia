89a7f6efb18a5359c7e47a5e726b380ab6cd5a9e
Dare Balogun <darebalogun@cmail.carleton.ca>
2020-04-22 01:45:09 -0400

    Persist highscore data with redux-persist and local storage

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React, { Component } from "react";
-import { StyleSheet, View, Image, Dimensions } from "react-native";
+import {
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  AsyncStorage,
+} from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createStore, combineReducers } from "redux";
 import { Provider } from "react-redux";
+import { persistStore, persistReducer } from "redux-persist";
+import { PersistGate } from "redux-persist/integration/react";
 
 import QuestionPage from "./components/QuestionPage";
 import StartPage from "./components/StartPage";
@@ -16,41 +24,51 @@ const rootReducer = combineReducers({
   highScore: highScoreReducer,
 });
 
-const store = createStore(rootReducer);
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+let store = createStore(persistedReducer);
+let persistor = persistStore(store);
 
 export default function App() {
   return (
     <Provider store={store}>
-      <NavigationContainer>
-        <Stack.Navigator
-          screenOptions={{
-            headerShown: false,
-            gesturesEnabled: false,
-          }}
-        >
-          <Stack.Screen
-            name="StartPage"
-            component={StartPage}
-            options={{
-              gestureEnabled: false,
+      <PersistGate loading={null} persistor={persistor}>
+        <NavigationContainer>
+          <Stack.Navigator
+            screenOptions={{
+              headerShown: false,
+              gesturesEnabled: false,
             }}
-          />
-          <Stack.Screen
-            name="QuestionPage"
-            component={QuestionPage}
-            options={{
-              gestureEnabled: false,
-            }}
-          />
-          <Stack.Screen
-            name="EndPage"
-            component={EndPage}
-            options={{
-              gestureEnabled: false,
-            }}
-          />
-        </Stack.Navigator>
-      </NavigationContainer>
+          >
+            <Stack.Screen
+              name="StartPage"
+              component={StartPage}
+              options={{
+                gestureEnabled: false,
+              }}
+            />
+            <Stack.Screen
+              name="QuestionPage"
+              component={QuestionPage}
+              options={{
+                gestureEnabled: false,
+              }}
+            />
+            <Stack.Screen
+              name="EndPage"
+              component={EndPage}
+              options={{
+                gestureEnabled: false,
+              }}
+            />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </PersistGate>
     </Provider>
   );
 }
